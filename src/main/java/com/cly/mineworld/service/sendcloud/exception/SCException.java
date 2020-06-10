be0f62cf15d03f1fcbd3d1b14941f2f22708ec97
Admire <l15012630328@163.com>
2020-06-10 22:49:11 +0800

    提交到gitHub中

--- src/main/java/com/cly/mineworld/service/sendcloud/exception/SCException.java
+++
@@ -1,6 +0,0 @@
-package com.cly.mineworld.service.sendcloud.exception;
-
-public interface SCException {
-
-	public String getMessage();
-}
--- src/main/java/com/cly/mineworld/service/sendcloud/model/Content.java
+++
@@ -1,9 +0,0 @@
-package com.cly.mineworld.service.sendcloud.model;
-
-import com.cly.mineworld.service.sendcloud.exception.ContentException;
-
-public interface Content {
-	public boolean useTemplate();
-
-	public boolean validate() throws ContentException;
-}
